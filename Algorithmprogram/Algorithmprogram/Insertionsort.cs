2387864dfb397c9b7380416f1d4ffe82606ccdd1
shital bramhe <shitalbramhe@gmail.com>
2021-11-25 17:55:28 +0530

    [shital] ADD: insertion sort

---
+++ Algorithmprogram/Algorithmprogram/Insertionsort.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithmprogram
+{
+    public class Insertionsort
+    {
+        public void Display(string[] array)
+        {
+            foreach (string item in array)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        public string[] Insertsort(string[] array)
+        {
+            for (int i = 1; i < array.Length; i++)
+            {
+                string temp = array[i];
+                int j = i - 1;
+                while (j >= 0 && array[j].CompareTo(temp) > 0)
+                {
+                    array[j + 1] = array[j];
+                    j--;
+                }
+                array[j + 1] = temp;
+            }
+            return array;
+        }
+    }
+}
---
+++ Algorithmprogram/Algorithmprogram/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Algorithmprogram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Enter Program number to get executed \n1. Insertion sort \n2. Exit");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        string[] array = { "hello", "how", "are", "you" };
+                        Console.WriteLine("Unsorted Array");
+                        Insertionsort obj = new Insertionsort();
+                        obj.Display(array);
+                        Console.WriteLine("Sorted array");
+                        string[] sortedarray = obj.Insertsort(array);
+                        obj.Display(sortedarray);
+                        break;
+                    case 2:
+                        flag = false;
+                        break;
+                    default:
+                        Console.WriteLine("invalid option");
+                        break;
+                }
+            }
+        }
+    }
+}
